0a6b27d7cd5096259fb00c104c417797ea914f3a
fukiwake <tak0203753@gmail.com>
2021-03-29 08:12:33 +0000

    fix migration

--- db/migrate/20210328012726_change_report.rb
+++
@@ -1,7 +0,0 @@
-class ChangeReport < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reports, :visitor_id, :integer
-    add_column :reports, :visited_id, :integer
-    remove_column :reports, :customer_id, :integer
-  end
-end
