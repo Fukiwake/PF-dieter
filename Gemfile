f41551ba09ea1c9ad2cffc8ccf1f52e955297776
fukiwake <tak0203753@gmail.com>
2021-03-30 09:55:22 +0000

    update layout

--- Gemfile
+++ Gemfile
@@ -86,6 +86,8 @@ gem 'ransack'
 gem 'simple_calendar', '~> 2.0'
 gem 'gon'
 gem 'rubocop-airbnb'
+gem 'google-cloud-vision'
+gem "google-cloud-translate"
 group :production do
   gem 'mysql2'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_002356) do
+ActiveRecord::Schema.define(version: 2021_03_30_023356) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2021_03_30_002356) do
     t.integer "genre", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "customer_id"
     t.text "body"
   end
 
