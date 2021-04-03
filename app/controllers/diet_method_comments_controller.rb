e0b85d4d024a42afdf92a888d46c186adfa59806
fukiwake <tak0203753@gmail.com>
2021-04-03 02:06:13 +0000

    finish ダイエット方法コメント返信

--- app/controllers/diet_method_comments_controller.rb
+++ app/controllers/diet_method_comments_controller.rb
@@ -2,25 +2,29 @@ class DietMethodCommentsController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-    diet_method_comment = current_customer.diet_method_comments.new(diet_method_comment_params)
+    @diet_method_comment = current_customer.diet_method_comments.new(diet_method_comment_params)
     @diet_method = DietMethod.find(params[:diet_method_id])
-    diet_method_comment.diet_method_id = @diet_method.id
-    diet_method_comment.save
-    @diet_method.create_notification_comment(current_customer, diet_method_comment.id)
+    @diet_method_comment.diet_method_id = @diet_method.id
+    @diet_method_comment.save
+    @diet_method.create_notification_comment(current_customer, @diet_method_comment.id)
     respond_to do |format|
       format.html { redirect_to request.referer }
       format.js
     end
+    @diet_method_comment_reply = DietMethodComment.new
   end
 
   def destroy
     @diet_method = DietMethod.find(params[:diet_method_id])
-    DietMethodComment.find_by(id: params[:id], diet_method_id: params[:diet_method_id]).destroy
+    @diet_method_comment = DietMethodComment.find_by(id: params[:id], diet_method_id: params[:diet_method_id])
+    @parent_id = @diet_method_comment.parent_id
+    @diet_method_comment.destroy
+    @diet_method_comment_reply = DietMethodComment.new
   end
 
   private
 
   def diet_method_comment_params
-    params.require(:diet_method_comment).permit(:body)
+    params.require(:diet_method_comment).permit(:body, :parent_id)
   end
 end
